caf1e10b5750bf55aa5b4fc168fc41686f23996c
ashfiaislam <ashfia.zara@gmail.com>
2019-04-27 16:29:26 +0000

    x

--- problemSetDay9.py
+++ problemSetDay9.py
@@ -1,1 +1,8 @@
 # File name: problemSetDay9.py
+import random
+list1 = ["mother", "father", "sister"]
+word_length = len(list1)
+random_index = random.randint(0,word_length-1)
+print(list1[random_index])
+word = list1[random_index]
+word_listversion = list(word)
